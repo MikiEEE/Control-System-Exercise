dc47910709c01cecbb50b83a86335c1c3a14e852
MikiEEE <mikemp1997@gmail.com>
2020-12-04 21:20:52 -0600

    Commented battery

--- Errors.py
+++ Errors.py
@@ -1,7 +1,16 @@
 
 
 class Max_Discharge(Exception):
+	'''
+	Raised in the event a battery is being asked to 
+	discharge more than it currently is able to.
+	'''
 	pass
 
+
 class Over_Charge(Exception):
+	'''
+	Raised when the battery is carrying more charge
+	than it's capacity.
+	'''
 	pass
--- models/Battery.py
+++ models/Battery.py
@@ -2,15 +2,34 @@ from Errors import Max_Discharge, Over_Charge
 
 class Battery():
 
-    def __init__(self,current_store,max_store):
-        if current_store > max_store:
+    def __init__(self,kwh_store,max):
+        '''
+        @param kwh_store - float - The amount of charge the 
+                battery currently carries. 
+        @param max - float - The max amount of capacity the 
+                battery can carry.
+        '''
+
+        if kwh_store > max:
             raise Over_Charge()
 
-        self.kwh_store = current_store
-        self.max = max_store
+        self.kwh_store = kwh_store
+        self.max = max
 
 
     def discharge(self, total_usage, threshold):
+        '''
+        @function discharge() - Subtracts the amount of usage over threshold
+                from the charge of the battery. If more KWH is needed than
+                the battery currently carries then a Max_Discharge error is 
+                raised.
+        @param total_usage - float - The amount of power usage in KWH.
+        @param threshold - float - The threshold that determines when the
+                battery should 'kick in' and discharge to supplement the 
+                usage.
+        @return - float - The amount of drain the battery has incurred. 
+        '''
+
         if total_usage > threshold:
             drain = total_usage - threshold
 
@@ -23,6 +42,18 @@ class Battery():
 
 
     def charge(self, total_usage, threshold):
+        '''
+        @function charge() - Adds them amount of usage under the threshold 
+                to the charge of the battery. If the battery is already
+                charged to full capacity, no charge is taken. 
+        @param total_usage - float - The amount of power usage in KWH.
+        @param threshold - float - The threshold that determines when the
+                battery should 'kick in' and charge to supplement the 
+                usage.
+        @return - float - The amount of additional usage the battery used 
+                to replenish it's KWH stores.
+        '''
+
         if total_usage < threshold:
             charge = threshold - total_usage
             if self.kwh_store == self.max:
@@ -38,6 +69,13 @@ class Battery():
 
 
     def refresh(self):
+        '''
+        @function refresh() - Resets the battery's charge store
+            the max value. Essentially charging the battery to 
+            100%. 
+        @return - void.
+        '''
+
         self.kwh_store = self.max
         return
 
